0e4dc554300cd3138366209ccdf7e22757d5961e
bmenez <bmenez@laposte.net>
2019-05-10 21:50:31 +0200

    filter

--- src/ListItems.js
+++ src/ListItems.js
@@ -3,30 +3,55 @@ import PropTypes from 'prop-types'
 
 class ListItems extends Component {
 
+    state = {
+        query: ''
+    }
+    
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+
     render() {
 
+        const { query } = this.state
         const { items, onDeleteItem } = this.props
+        
+        const showingItems = query === ''
+            ? items
+            : items.filter((item) => (
+                item.firstName.toLowerCase().includes(query.toLowerCase())
+            ))
 
         return (
-          <ul className='item-list'>
-            {items.map((item) => (
-                <li key={item.id} className='list-item'>
-                    <span className='item-firstName'>
-                        {item.firstName}
-                    </span>
-                    &nbsp;
-                    <span className='item-lastName'>
-                        {item.lastName}
-                    </span>
-                    &nbsp;
-                    <button
-                        onClick={() => onDeleteItem(item)}
-                        className='item-delete'>
-                        Delete
-                    </button>
-                </li>
-            ))}
-          </ul>
+            <div className='item-list'>
+                <input
+                    className='search-items'
+                    type='text'
+                    value={this.state.query}
+                    onChange={(event) => this.updateQuery(event.target.value)}
+                />
+                <ul>
+                    {showingItems.map((item) => (
+                        <li key={item.id} className='list-item'>
+                            <span className='item-firstName'>
+                                {item.firstName}
+                            </span>
+                            &nbsp;
+                            <span className='item-lastName'>
+                                {item.lastName}
+                            </span>
+                            &nbsp;
+                            <button
+                                onClick={() => onDeleteItem(item)}
+                                className='item-delete'>
+                                Delete
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>    
         )
 
     }
